377f19f8ea22f7b100d7825bea06255c86bf8e3b
jomo34 <trillion32@naver.com>
2023-07-01 16:13:08 +0900

    사이드바 초기 설정

--- src/components/SideDetailBar/index.tsx
+++ src/components/SideDetailBar/index.tsx
@@ -1,10 +1,80 @@
-import { Bar } from "@components/SideDetailBar/styles.tsx";
+import {
+    Wrapper,
+    DropdownDiv,
+    HorizontalLine,
+    ScrollWrapper,
+    ScrollDiv
+} from "@components/SideDetailBar/styles.tsx";
+import { DownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
+import {Link} from "react-router-dom";
+import {useRef} from "react";
+///https://ant.design/components/list
+
 
 const SideDetailBar = () => {
-  return (
-    <>
-      <Bar>asdassdsdd</Bar>
-    </>
+    const boxRef = useRef(null);
+    const items: MenuProps['items'] = [
+        {
+            key: '1',
+            label: (
+                <Link to="/mypage">
+                    프로젝트 정보
+                </Link>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <Link to="/mypage">
+                    멤버 관리
+                </Link>
+            ),
+        },
+        {
+            key: '3',
+            danger: true,
+            label: (
+                <Link to="/mypage">
+                    프로젝트 나가기
+                </Link>
+            ),
+        },
+    ];
+    return (
+      <Wrapper>
+          <DropdownDiv>
+              <Dropdown menu={{ items }} trigger={['click']}>
+                  <a onClick={(e) => e.preventDefault()}>
+                      <Space>
+                          프로젝트 명
+                          <DownOutlined />
+                      </Space>
+                  </a>
+              </Dropdown><HorizontalLine/>
+          </DropdownDiv>
+
+          <ScrollWrapper>
+              <text>릴리즈 노트</text>
+              <HorizontalLine />
+              <ScrollDiv ref={boxRef}>
+              </ScrollDiv>
+          </ScrollWrapper>
+          <ScrollWrapper>
+              <text>작업 관리</text>
+              <HorizontalLine />
+              <ScrollDiv ref={boxRef}>
+              </ScrollDiv>
+          </ScrollWrapper>
+          <ScrollWrapper>
+              <text>이슈</text>
+              <HorizontalLine />
+              <ScrollDiv ref={boxRef}>
+              </ScrollDiv>
+          </ScrollWrapper>
+      </Wrapper>
+
   );
 };
 
--- src/components/SideDetailBar/styles.tsx
+++ src/components/SideDetailBar/styles.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-export const Bar = styled.div`
+export const Wrapper = styled.div`
   background-color: skyblue;
   width: 20%;
   height: 100%;
@@ -8,4 +8,26 @@ export const Bar = styled.div`
   top: 7%;
   left: 8%;
   z-index: 11;
+`;
+
+export const DropdownDiv = styled.div`
+  height: 1rem;
+  align-items: center;
+  padding: 5%;
+`;
+
+export const HorizontalLine = styled.div`
+  border-bottom: 1px solid #aaa;
+  line-height: 1rem;
+  margin-top: 0.5rem;
+`;
+
+export const ScrollWrapper = styled.div`
+  margin-top: 3rem;
+  padding: 5%;
+`;
+
+export const ScrollDiv = styled.div`
+  margin-top: 0.5rem;
+  background-color: yellow;
 `;
