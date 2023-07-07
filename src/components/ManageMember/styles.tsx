ea452e565bba2a8a080059cb9495d7efad5b9b8c
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-07-07 16:31:25 +0900

    stlye: 프로젝트 정보 스타일 생성

--- src/components/ManageMember/styles.tsx
+++ src/components/ManageMember/styles.tsx
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
 
+export const ProjectSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin: 0 1rem;
+`;
 export const MemberSection = styled.div`
   display: flex;
   flex-direction: column;
@@ -7,6 +13,18 @@ export const MemberSection = styled.div`
   margin: 0 1rem;
 `;
 
+export const ProjectHeader = styled.div`
+  font-size: 1.5rem;
+  padding-top: 2rem;
+`;
+export const ProjectBody = styled.div``;
+export const ProjectSubMit = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ProjectBodyTitle = styled.div``;
+export const ProjectBodyExplain = styled.div``;
 export const MemberHeader = styled.div`
   display: flex;
   justify-content: space-between;
