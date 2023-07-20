04427ec79198ba83fc5f2891e298ec86dc20c3c5
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-07-20 13:05:11 +0900

    refactor: 변수 이름 변경

--- src/components/Issue/index.tsx
+++ src/components/Issue/index.tsx
@@ -7,23 +7,23 @@ import { IssueCreateBtn, IssueHeader, IssueTable } from '@components/Issue/style
 
 interface DataType {
   key: React.Key;
+  issue: string;
+  version: string;
   name: string;
-  email: number;
-  position: string;
 }
 
 const columns: ColumnsType<DataType> = [
   {
-    title: '이름',
+    title: '이슈 제목',
+    dataIndex: 'issue',
+  },
+  {
+    title: '버전',
+    dataIndex: 'version',
+  },
+  {
+    title: '작성자',
     dataIndex: 'name',
-  },
-  {
-    title: '이메일',
-    dataIndex: 'email',
-  },
-  {
-    title: '직위',
-    dataIndex: 'position',
   },
 ];
 const Issue = () => {
@@ -33,9 +33,9 @@ const Issue = () => {
   for (let i = 0; i < 10; i++) {
     data.push({
       key: i,
-      name: `이름입니다 ${i}`,
-      email: 32,
-      position: `주소 ${i}`,
+      issue: `이슈 제목 ${i}`,
+      version: `${i}`,
+      name: `사람 이름 ${i}`,
     });
   }
 
