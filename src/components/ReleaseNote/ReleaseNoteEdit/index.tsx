d765ed909033d5f88eda523465c23d58de083b57
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-07-27 21:16:18 +0900

    refactor : 사이드바 애니메이션 useEffect 삭제 및 코드 성능 개선

--- src/components/ReleaseNote/ReleaseNoteEdit/index.tsx
+++ src/components/ReleaseNote/ReleaseNoteEdit/index.tsx
@@ -1,5 +1,5 @@
 import MDEditor from '@uiw/react-md-editor';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button, Form, Input } from 'antd';
 import HeaderBar from '@components/HeaderBar';
 import SideBar from '@components/SideBar';
@@ -8,7 +8,7 @@ import { Wrapper } from '@styles/DetailSide/styles.tsx';
 import { ReleasedNoteDiv } from '@components/ReleaseNote/ReleasedNoteAll/styles.tsx';
 import { Editor, TemporarySave, TitleVersionInput } from '@components/ReleaseNote/ReleaseNoteEdit/styles.tsx';
 import { projectInfoMenuOpenState } from '@states/ProjectState.ts';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValueLoadable } from 'recoil';
 import { ActivityIndicator } from '@components/ActivityIndicator';
 
 interface FormValues {
@@ -18,17 +18,7 @@ interface FormValues {
 const ReleaseNoteEdit = () => {
   const [value, setValue] = useState<string | undefined>('**내용을 입력해주세요.**');
   const [form] = Form.useForm();
-  const projectInfoMenuOpen = useRecoilValue(projectInfoMenuOpenState);
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    if (!projectInfoMenuOpen) {
-      setIsVisible(false);
-      setTimeout(() => {
-        setIsVisible(true);
-      }, 550);
-    }
-  }, [projectInfoMenuOpen]);
+  const projectInfoMenuOpen = useRecoilValueLoadable(projectInfoMenuOpenState);
 
   const handleSubmit = (values: FormValues) => {
     console.log(values);
@@ -38,39 +28,61 @@ const ReleaseNoteEdit = () => {
     version: '1.0.0',
     title: '미리 작성된 제목',
   };
+
+  let contents = null;
+
+  switch (projectInfoMenuOpen.state) {
+    case 'hasValue':
+      contents = () => {
+        if (projectInfoMenuOpen.contents) {
+          return (
+            <ReleasedNoteDiv>
+              <TitleVersionInput>
+                <Form initialValues={initialValues} form={form} onFinish={handleSubmit}>
+                  <Form.Item name="version" label="버전" rules={[{ required: true, message: '버전을 입력해주세요' }]}>
+                    <Input />
+                  </Form.Item>
+                  <Form.Item name="title" label="제목" rules={[{ required: true, message: '제목을 입력해주세요' }]}>
+                    <Input />
+                  </Form.Item>
+                </Form>
+              </TitleVersionInput>
+              <Editor data-color-mode="light">
+                <MDEditor height={'60vh'} value={value} onChange={setValue} />
+              </Editor>
+              <TemporarySave>
+                <Button type="primary" onClick={() => form.submit()}>
+                  임시저장
+                </Button>
+              </TemporarySave>
+            </ReleasedNoteDiv>
+          );
+        } else {
+          return <ActivityIndicator />;
+        }
+      };
+      break;
+    case 'hasError':
+      contents = () => {
+        return <div>데이터를 서버에서 불러올 수 없습니다.</div>;
+      };
+      break;
+    case 'loading':
+      contents = () => {
+        return <ActivityIndicator />;
+      };
+      break;
+    default:
+      contents = () => {
+        return <div>에러가 발생했습니다. 페이지를 새로고침해주세요.</div>;
+      };
+  }
   return (
     <>
       <HeaderBar />
       <SideBar />
       <SideDetailBar />
-      {isVisible ? (
-        <Wrapper>
-          <ReleasedNoteDiv>
-            <TitleVersionInput>
-              <Form initialValues={initialValues} form={form} onFinish={handleSubmit}>
-                <Form.Item name="version" label="버전" rules={[{ required: true, message: '버전을 입력해주세요' }]}>
-                  <Input />
-                </Form.Item>
-                <Form.Item name="title" label="제목" rules={[{ required: true, message: '제목을 입력해주세요' }]}>
-                  <Input />
-                </Form.Item>
-              </Form>
-            </TitleVersionInput>
-            <Editor data-color-mode="light">
-              <MDEditor height={'60vh'} value={value} onChange={setValue} />
-            </Editor>
-            <TemporarySave>
-              <Button type="primary" onClick={() => form.submit()}>
-                임시저장
-              </Button>
-            </TemporarySave>
-          </ReleasedNoteDiv>
-        </Wrapper>
-      ) : (
-        <Wrapper>
-          <ActivityIndicator />
-        </Wrapper>
-      )}
+      <Wrapper>{contents()}</Wrapper>
     </>
   );
 };
