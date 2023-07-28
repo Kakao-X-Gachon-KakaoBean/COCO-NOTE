f90ec1662e060e4ccfbea13a33f0c03cbc285ac6
jomo34 <trillion32@naver.com>
2023-07-28 15:55:33 +0900

    feat: 프로젝트 정보 수정 기능 구현

--- src/components/ManageMember/index.tsx
+++ src/components/ManageMember/index.tsx
@@ -4,15 +4,15 @@ import {
   MemberHeaderRight,
   MemberList,
   MemberSection,
-  ProjectSection,
   ProjectBody,
-  ProjectHeader,
-  ProjectSubMit,
-  ProjectBodyTitle,
   ProjectBodyExplain,
+  ProjectBodyTitle,
+  ProjectHeader,
+  ProjectSection,
+  ProjectSubMit,
 } from '@components/ManageMember/styles.tsx';
 
-import { Select, Button, Input, Modal, Divider } from 'antd';
+import { Button, Divider, Input, Modal, Select } from 'antd';
 
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -29,17 +29,17 @@ import FirstPageIcon from '@mui/icons-material/FirstPage';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import LastPageIcon from '@mui/icons-material/LastPage';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Wrapper } from '@styles/DetailSide/styles.tsx';
-import HeaderBar from '@components/HeaderBar';
-import SideBar from '@components/SideBar';
 import { CloseOutlined } from '@ant-design/icons';
 import { TableHead } from '@mui/material';
 import useInput from '../../hooks/useInput.ts';
 import axios, { AxiosError } from 'axios';
 import { useMutation } from 'react-query';
 import { MemberState } from '@states/MemberState.ts';
-import SideDetailBar from '@components/SideDetailBar';
+import { projectInfoMenuOpenState, SelectedProjectState } from '@states/ProjectState.ts';
+import { useRecoilValue } from 'recoil';
+import { ActivityIndicator } from '@components/ActivityIndicator';
 
 interface TablePaginationActionsProps {
   count: number;
@@ -101,9 +101,10 @@ const ManageMember = () => {
   const [projectModalOpen, SetProjectModalOpen] = useState(false);
   const [email, onChangeEmail, setEmail] = useInput('');
   const [emails, setEmails] = useState<string[]>([]);
-  const [title, setTitle] = useState<string>('코코노트');
-  const [contents, setContents] = useState<string>('효과적 으아아아');
+  const selectedProject = useRecoilValue(SelectedProjectState);
   const { TextArea } = Input;
+  const [title, setTitle] = useState(selectedProject.projectTitle);
+  const [content, setContent] = useState(selectedProject.projectContent);
 
   const [rows, setRows] = useState([
     { name: '추성준', email: 'j949854@gmail.com', position: '관리자' },
@@ -136,6 +137,17 @@ const ManageMember = () => {
       label: '구경',
     },
   ];
+  const projectInfoMenuOpen = useRecoilValue(projectInfoMenuOpenState);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (!projectInfoMenuOpen) {
+      setIsVisible(false);
+      setTimeout(() => {
+        setIsVisible(true);
+      }, 550);
+    }
+  }, [projectInfoMenuOpen]);
 
   const handleChange = (value: { value: string; label: React.ReactNode }, i: number) => {
     const newRows = [...rows];
@@ -210,184 +222,205 @@ const ManageMember = () => {
     setPage(0);
   };
 
+  const modifyProject = () => {
+    const newProject: IProjectValue = {
+      ...selectedProject, // Keep other properties of the selected project unchanged
+      projectTitle: title,
+      projectContent: content,
+    };
+    const selectedIndex = projectList.findIndex(project => project.projectId === selectedProject.projectId);
+
+    // Create a new array with the modified project
+    const updatedProjectList = [...projectList];
+    updatedProjectList[selectedIndex] = newProject;
+
+    // Update the entire projectValueState with the new array
+    setProjectList(updatedProjectList);
+    setSelectedProject(newProject);
+    console.log(selectedIndex);
+    console.log(updatedProjectList);
+  };
+
+  const handleOk = () => {
+    if (title && content && (title !== selectedProject.projectTitle || content !== selectedProject.projectContent)) {
+      modifyProject();
+      SetProjectModalOpen(false);
+      toast.success('프로젝트가 생성 되었습니다.'); // toast.success로 성공 메시지 표시
+    } else {
+      toast.error('프로젝트 명과 프로젝트 설명을 다시 확인해주세요'); // toast.error로 실패 메시지 표시
+    }
+  };
+
   return (
     <>
-      {/*<HeaderBar />*/}
-      {/*<SideBar />*/}
-      {/*<SideDetailBar />*/}
-      <Wrapper>
-        <ProjectSection>
-          <Button type="primary" shape="circle" icon={<CloseOutlined />} />
-          <ProjectHeader>
-            <div>프로젝트 정보</div>
-            <div>
-              <Button type="primary" size="large" onClick={() => SetProjectModalOpen(true)}>
-                프로젝트 정보 수정
-              </Button>
-            </div>
-          </ProjectHeader>
-          <ProjectBody>
-            <ProjectBodyTitle>
-              <div>프로젝트 이름</div>
-              <div>{title}</div>
-            </ProjectBodyTitle>
-            <ProjectBodyExplain>
-              <div>프로젝트 설명</div>
-              <div>{contents}</div>
-            </ProjectBodyExplain>
-          </ProjectBody>
-          <ProjectSubMit></ProjectSubMit>
-        </ProjectSection>
-        <Divider />
-        <MemberSection>
-          <MemberHeader>
-            <MemberHeaderLeft>구성원 관리</MemberHeaderLeft>
-            <MemberHeaderRight>
-              <Button
-                type="primary"
-                size="large"
-                style={{ color: 'black', backgroundColor: 'white', border: '1px' }}
-                onClick={() => SetInvitationModalOpen(true)}
-              >
-                멤버 추가
-              </Button>
-              <Button type="primary" style={{ color: 'white' }} size={'large'}>
-                멤버 권한 저장
-              </Button>
-            </MemberHeaderRight>
-          </MemberHeader>
-          <MemberList>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-                <TableHead>
-                  <TableRow sx={{ background: '#f5f5f8' }}>
-                    <TableCell align="left">이름</TableCell>
-                    <TableCell align="center">이메일</TableCell>
-                    <TableCell align="center">직위</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {(rowsPerPage > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : rows).map(
-                    (row, i) => (
-                      <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell style={{ width: 300 }} align="center">
-                          {row.email}
-                        </TableCell>
-                        <TableCell style={{ width: 160, paddingRight: 16 }} align="center">
-                          <Select
-                            labelInValue
-                            defaultValue={{
-                              value: row.position,
-                              label: row.position,
-                            }}
-                            style={{ width: 80, marginRight: 10 }}
-                            onChange={value => handleChange(value, i)}
-                            options={SelectOption}
-                          />
-                          <CloseOutlined onClick={() => handleDelete(i)} />
-                        </TableCell>
-                      </TableRow>
-                    )
-                  )}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={6} />
-                    </TableRow>
-                  )}
-                </TableBody>
-                <TableFooter>
-                  <TableRow>
-                    <TablePagination
-                      rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                      colSpan={3}
-                      count={rows.length}
-                      rowsPerPage={rowsPerPage}
-                      page={page}
-                      SelectProps={{
-                        inputProps: {
-                          'aria-label': 'rows per page',
-                        },
-                        native: true,
-                      }}
-                      onPageChange={handleChangePage}
-                      onRowsPerPageChange={handleChangeRowsPerPage}
-                      ActionsComponent={TablePaginationActions}
-                    />
-                  </TableRow>
-                </TableFooter>
-              </Table>
-            </TableContainer>
-          </MemberList>
-          <Button type="primary" danger size={'large'}>
-            프로젝트 삭제
-          </Button>
-        </MemberSection>
-        <Modal
-          title="인원 추가히기"
-          open={invitationModalOpen}
-          onCancel={handleInvitation}
-          footer={
-            <Button type="primary" style={{ width: '5rem' }} onClick={onSubmitEmail}>
-              전송
-            </Button>
-          }
-          centered
-        >
-          <p>이메일 입력</p>
-          <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-            <Input type="text" value={email} onChange={onChangeEmail} placeholder="이메일" />
-            <div>
-              {emails.map((email, index) => (
-                <div
-                  key={index}
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                  }}
+      {isVisible ? (
+        <Wrapper>
+          <ProjectSection>
+            <Button type="primary" shape="circle" icon={<CloseOutlined />} />
+            <ProjectHeader>
+              <div>프로젝트 정보</div>
+              <div>
+                <Button type="primary" size="large" onClick={() => SetProjectModalOpen(true)}>
+                  프로젝트 정보 수정
+                </Button>
+              </div>
+            </ProjectHeader>
+            <ProjectBody>
+              <ProjectBodyTitle>
+                <div>프로젝트 이름</div>
+                <div>{selectedProject.projectTitle}</div>
+              </ProjectBodyTitle>
+              <ProjectBodyExplain>
+                <div>프로젝트 설명</div>
+                <div>{selectedProject.projectContent}</div>
+              </ProjectBodyExplain>
+            </ProjectBody>
+            <ProjectSubMit></ProjectSubMit>
+          </ProjectSection>
+          <Divider />
+          <MemberSection>
+            <MemberHeader>
+              <MemberHeaderLeft>구성원 관리</MemberHeaderLeft>
+              <MemberHeaderRight>
+                <Button
+                  type="primary"
+                  size="large"
+                  style={{ color: 'black', backgroundColor: 'white', border: '1px' }}
+                  onClick={() => SetInvitationModalOpen(true)}
                 >
-                  <div style={{ marginRight: '1rem' }}>{email}</div>
-                  <CloseOutlined onClick={() => deleteEmails(index)} />
-                </div>
-              ))}
-            </div>
-
-            <Button type="primary" onClick={addEmail}>
-              추가
+                  멤버 추가
+                </Button>
+                <Button type="primary" style={{ color: 'white' }} size={'large'}>
+                  멤버 권한 저장
+                </Button>
+              </MemberHeaderRight>
+            </MemberHeader>
+            <MemberList>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+                  <TableHead>
+                    <TableRow sx={{ background: '#f5f5f8' }}>
+                      <TableCell align="left">이름</TableCell>
+                      <TableCell align="center">이메일</TableCell>
+                      <TableCell align="center">직위</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {(rowsPerPage > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : rows).map(
+                      (row, i) => (
+                        <TableRow key={row.name}>
+                          <TableCell component="th" scope="row">
+                            {row.name}
+                          </TableCell>
+                          <TableCell style={{ width: 300 }} align="center">
+                            {row.email}
+                          </TableCell>
+                          <TableCell style={{ width: 160, paddingRight: 16 }} align="center">
+                            <Select
+                              labelInValue
+                              defaultValue={{
+                                value: row.position,
+                                label: row.position,
+                              }}
+                              style={{ width: 80, marginRight: 10 }}
+                              onChange={value => handleChange(value, i)}
+                              options={SelectOption}
+                            />
+                            <CloseOutlined onClick={() => handleDelete(i)} />
+                          </TableCell>
+                        </TableRow>
+                      )
+                    )}
+                    {emptyRows > 0 && (
+                      <TableRow style={{ height: 53 * emptyRows }}>
+                        <TableCell colSpan={6} />
+                      </TableRow>
+                    )}
+                  </TableBody>
+                  <TableFooter>
+                    <TableRow>
+                      <TablePagination
+                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                        colSpan={3}
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        SelectProps={{
+                          inputProps: {
+                            'aria-label': 'rows per page',
+                          },
+                          native: true,
+                        }}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                        ActionsComponent={TablePaginationActions}
+                      />
+                    </TableRow>
+                  </TableFooter>
+                </Table>
+              </TableContainer>
+            </MemberList>
+            <Button type="primary" danger size={'large'}>
+              프로젝트 삭제
             </Button>
-          </div>
-        </Modal>
-        <Modal
-          title="프로젝트 정보 변경"
-          open={projectModalOpen}
-          onCancel={handleProject}
-          footer={
-            <div>
-              <Button key="submit" type="primary">
-                OK
+          </MemberSection>
+          <Modal
+            title="인원 추가히기"
+            open={invitationModalOpen}
+            onCancel={handleInvitation}
+            footer={
+              <Button type="primary" style={{ width: '5rem' }} onClick={onSubmitEmail}>
+                전송
+              </Button>
+            }
+            centered
+          >
+            <p>이메일 입력</p>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+              <Input type="text" value={email} onChange={onChangeEmail} placeholder="이메일" />
+              <div>
+                {emails.map((email, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <div style={{ marginRight: '1rem' }}>{email}</div>
+                    <CloseOutlined onClick={() => deleteEmails(index)} />
+                  </div>
+                ))}
+              </div>
+
+              <Button type="primary" onClick={addEmail}>
+                추가
               </Button>
             </div>
-          }
-        >
-          <TextArea
-            value={title}
-            autoSize={{ minRows: 1, maxRows: 10 }}
-            onChange={e => setTitle(e.target.value)}
-            placeholder="프로젝트 명"
-            style={{ marginBottom: '2rem', marginTop: '3rem' }}
-          />
-          <TextArea
-            value={contents}
-            autoSize={{ minRows: 3, maxRows: 10 }}
-            onChange={e => setContents(e.target.value)}
-            placeholder="프로젝트 설명"
-            style={{ marginBottom: '2rem' }}
-          />
-        </Modal>
-      </Wrapper>
+          </Modal>
+          <Modal title="새 프로젝트 생성" open={projectModalOpen} onOk={handleOk} onCancel={handleProject}>
+            <TextArea
+              value={title}
+              autoSize={{ minRows: 1, maxRows: 10 }}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="프로젝트 명"
+              style={{ marginBottom: '2rem', marginTop: '3rem' }}
+            />
+            <TextArea
+              value={content}
+              autoSize={{ minRows: 3, maxRows: 10 }}
+              onChange={e => setContent(e.target.value)}
+              placeholder="프로젝트 설명"
+              style={{ marginBottom: '2rem' }}
+            />
+          </Modal>
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <ActivityIndicator />
+        </Wrapper>
+      )}
     </>
   );
 };
