93ee0f247391ee0d63d86b10c7336447279c59a2
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-01 15:43:31 +0900

    chore: 패키지 설정 변경

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs', '@babel/plugin-transform-runtime'],
+    },
+  },
 };
