934bf398a64a5ba83d0ba3398dbe1d4013bd52d3
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-02 15:12:37 +0900

    feat: 로그인 로그아웃 버튼 구현

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -4,9 +4,23 @@ import MyInfo from '@components/HeaderBar/MyInfo';
 import { useRecoilState } from 'recoil';
 import { projectInfoMenuOpenState } from '@states/ProjectState.ts';
 import { useNavigate } from 'react-router-dom';
+import { useCallback, useState } from 'react';
 
 const HeaderBar = () => {
   const [projectInfoMenuOpen, setProjectInfoMenuOpen] = useRecoilState(projectInfoMenuOpenState);
+  const [isLogin, setIsLogin] = useState(localStorage.getItem('accessToken') !== null);
+
+  const redirectLogin = useCallback(() => {
+    document.location.href = '/login';
+  }, []);
+
+  const onLogout = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    localStorage.removeItem('accessToken');
+    setIsLogin(false);
+    document.location.href = '/';
+  }, []);
+
   const navigate = useNavigate();
   function waitForAnimation() {
     return new Promise(resolve => setTimeout(resolve, 550));
@@ -28,6 +42,7 @@ const HeaderBar = () => {
           </TitleLink>
         </LogoDiv>
         <OthersDiv>
+          <div>{!isLogin ? <div onClick={redirectLogin}>LogIn</div> : <div onClick={onLogout}>Logout</div>}</div>
           <Notification />
           <MyInfo />
         </OthersDiv>
