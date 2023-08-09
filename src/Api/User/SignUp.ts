5fd8ce16605f3407881ebcc61724c0b241f1d187
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-09 13:25:54 +0900

    feat: 회원가입 api 분리 완료

--- src/Api/User/SignUp.ts
+++ src/Api/User/SignUp.ts
@@ -1,1 +1,30 @@
 //회원가입 페이지
+
+import { instance } from '@/Api';
+import { SignUpUser } from '@states/userState.ts';
+
+export const signUp = async (data: SignUpUser) => {
+  try {
+    await instance.post('/members', data);
+    return '회원가입 성공';
+  } catch (err) {
+    return '회원가입 실패';
+  }
+};
+
+export const postEmail = async (data: string) => {
+  try {
+    await instance.post(
+      '/emails',
+      { email: data },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    return '이메일 발송 성공';
+  } catch (err) {
+    return '이메일 발송 실패';
+  }
+};
