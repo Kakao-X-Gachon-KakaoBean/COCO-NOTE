5ee6acceccb09e2375f6d8b322053a146c5ced79
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-09 13:26:12 +0900

    feat: 로그인 api 분리중

--- src/Api/User/Login.ts
+++ src/Api/User/Login.ts
@@ -1,1 +1,16 @@
 //로그인 페이지
+
+import { instance } from '@/Api';
+import { AxiosResponse } from 'axios';
+import { LoginUser } from '@states/userState.ts';
+
+export const logIn = async (data: LoginUser): Promise<LoginUser> => {
+  try {
+    const res: AxiosResponse<LoginUser> = await instance.post('/local/login', data);
+
+    return res.data;
+  } catch (err) {
+    console.log('회원가입 실패');
+    throw err;
+  }
+};
