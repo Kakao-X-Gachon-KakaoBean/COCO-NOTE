ab6a963b51af60ef95813889fc93696229e253cd
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-10 14:58:19 +0900

    feat: 이슈 api 등록

--- src/Api/Issue/Issue.ts
+++ src/Api/Issue/Issue.ts
@@ -1,1 +1,22 @@
 //이슈 페이지 api
+
+import { instance } from '@/Api';
+import { CreateComment, CreateIssue } from '@states/IssueState.ts';
+
+export const postIssue = async (data: CreateIssue) => {
+  try {
+    await instance.post('/issues', data);
+    return '이슈 생성 완료';
+  } catch (err) {
+    return '이슈 생성 실패';
+  }
+};
+
+export const postComment = async (data: CreateComment) => {
+  try {
+    await instance.post('/comments', data);
+    return '댓글 달기 완료';
+  } catch (err) {
+    return '댓글 달기 실패';
+  }
+};
