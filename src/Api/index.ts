9fbaee1450051f50563c35f64e42ccb25804a114
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-14 13:48:07 +0900

    fix: accessToken 값 저장 안되는 버그 해결

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -8,3 +8,14 @@ export const instance = axios.create({
   withCredentials: true,
   headers: { Authorization: `Bearer ${accessToken}` },
 });
+
+instance.interceptors.request.use(
+  config => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
