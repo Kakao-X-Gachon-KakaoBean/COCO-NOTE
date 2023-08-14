9004ca90649922ec2a2533c7160652c51888620e
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-14 20:45:04 +0900

    fix: 사용하지 않는 변수 제거

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -40,7 +40,7 @@ const LogIn = () => {
   const [password, onChangePassword] = useInput('');
   const [memberId, setMemberId] = useRecoilState(memberIdState);
   const [checkPasswordModal, setCheckPasswordModal] = useState(false);
-  const [isLogin, setIsLogin] = useState(localStorage.getItem('accessToken') !== null);
+  const [isLogin] = useState(localStorage.getItem('accessToken') !== null);
   const navigate = useNavigate();
   const onClosePasswordModal = useCallback(() => {
     setCheckPasswordModal(prev => !prev);
