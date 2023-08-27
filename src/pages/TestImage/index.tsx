eebb440eed0a94dd0965c2f5208ac89f031a85d4
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-08-27 23:05:02 +0900

    feat: 토스트 유아이 종속성 문제 해결

--- src/pages/TestImage/index.tsx
+++ src/pages/TestImage/index.tsx
@@ -1,5 +1,6 @@
 import MDEditor from '@uiw/react-md-editor';
 import React, { useState } from 'react';
+import { Editor } from '@toast-ui/react-editor';
 
 const TestImage = () => {
   const [content, setContent] = useState<string | undefined>('**내용을 입력해주세요.**');
@@ -7,6 +8,15 @@ const TestImage = () => {
     <>
       <div data-color-mode="light">
         <MDEditor height={500} value={content} onChange={setContent} />
+      </div>
+      <div>
+        <Editor
+          height={'100%'}
+          initialEditType="wysiwyg"
+          hideModeSwitch={true}
+          useCommandShortcut={false}
+          language="ko-KR"
+        />
       </div>
     </>
   );
