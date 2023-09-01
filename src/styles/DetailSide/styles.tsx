db149c0cae79736d8c7743db3b19e5429b2e5afd
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-09-02 07:16:15 +0900

    refactor: hook 바깥 recoilValue 사용 삭제

--- src/styles/DetailSide/styles.tsx
+++ src/styles/DetailSide/styles.tsx
@@ -1,19 +1,12 @@
 import styled from '@emotion/styled';
 import { useRecoilValue } from 'recoil';
 import { projectInfoMenuOpenState } from '@/states/ProjectState.ts';
-
-const projectInfoMenuOpen = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const state = useRecoilValue(projectInfoMenuOpenState);
-  return state;
-};
-
 export const Wrapper = styled.div`
   width: 72%;
   height: 93vh;
   margin-top: 7%;
   margin-left: 28%;
   font-family: SCDream4;
-  transform: translateX(${() => (projectInfoMenuOpen() ? '0' : '-25%')});
+  transform: translateX(${() => (useRecoilValue(projectInfoMenuOpenState) ? '0' : '-25%')});
   transition: transform 0.3s ease-out;
 `;
