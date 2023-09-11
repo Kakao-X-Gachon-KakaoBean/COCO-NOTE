1f5bd0ccdf6805d447a0a87c9b6b78170f7c6a31
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-09-11 14:47:43 +0900

    feat: 배포 테스트

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -26,7 +26,7 @@ const HeaderBar = () => {
               navigate('/main');
             }}
           >
-            COCONOTE 테스트 테스트
+            COCONOTE 테스트 테스트 테스트 3트
           </TitleLink>
         </LogoDiv>
         <OthersDiv>
