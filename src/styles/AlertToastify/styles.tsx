68856e38df772ec0db98911ade1069edfefa4d5b
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-09-14 18:23:55 +0900

    refactor: alert toastify 스타일 지정

---
+++ src/styles/AlertToastify/styles.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+export const AlertToastDiv = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 14vw;
+  line-height: 1.5;
+  padding: 0;
+  height: auto;
+`;
