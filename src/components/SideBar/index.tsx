17c00c01396ba328a25b7c6d49df6c7c612112b9
jomo34 <trillion32@naver.com>
2023-09-27 00:14:37 +0900

    refactor: 로고 이미지는 고정, 그 아래만 스크롤되게 변경

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,4 @@
-import { AddCircle, Circle, InnerText, LogoImage, Wrapper } from '@/components/SideBar/styles.tsx';
+import { AddCircle, Circle, InnerText, LogoImage, ProjectWrapper, Wrapper } from '@/components/SideBar/styles.tsx';
 import { useRecoilState, useResetRecoilState } from 'recoil';
 import { AddProjectClickState, projectInfoMenuOpenState, SelectedProjectState } from '@/states/ProjectState.ts';
 import { useNavigate } from 'react-router-dom';
@@ -54,29 +54,30 @@ const SideBar = () => {
               alt={'logoImage'}
             ></LogoImage>
           </Tooltip>
-          {data.map(project => (
-            <div key={project.projectId}>
-              <Tooltip placement={'right'} title={project.projectTitle}>
-                <Circle
-                  className={selectedProject.projectId === project.projectId ? 'selected' : 'notSelected'}
-                  onClick={async () => {
-                    setSelectedProject(project);
-                    setProjectInfoMenuOpen(true);
-                    await waitForAnimation();
-                    navigate(`/projects/${project.projectId}/projectinfo`);
-                  }}
-                >
-                  <InnerText>{project.projectTitle}</InnerText>
-                </Circle>
-              </Tooltip>
-            </div>
-          ))}
+          <ProjectWrapper>
+            {data.map(project => (
+              <div key={project.projectId}>
+                <Tooltip placement={'right'} title={project.projectTitle}>
+                  <Circle
+                    className={selectedProject.projectId === project.projectId ? 'selected' : 'notSelected'}
+                    onClick={async () => {
+                      setSelectedProject(project);
+                      setProjectInfoMenuOpen(true);
+                      await waitForAnimation();
+                      navigate(`/projects/${project.projectId}/projectinfo`);
+                    }}
+                  >
+                    <InnerText>{project.projectTitle}</InnerText>
+                  </Circle>
+                </Tooltip>
+              </div>
+            ))}
+            <AddCircle onClick={() => setIsAddProject(true)}>
+              <InnerText style={{ fontSize: '2rem' }}>+</InnerText>
+            </AddCircle>
+          </ProjectWrapper>
         </div>
       )}
-
-      <AddCircle onClick={() => setIsAddProject(true)}>
-        <InnerText style={{ fontSize: '2rem' }}>+</InnerText>
-      </AddCircle>
     </Wrapper>
   );
 };
