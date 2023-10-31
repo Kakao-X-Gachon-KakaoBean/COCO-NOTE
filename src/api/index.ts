17a66088188a126fa8302418781f05e464bdf0d1
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-10-31 23:11:44 +0900

    refactor: env 방식 변경(테스트 위함)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const GOOGLE_URL = import.meta.env.VITE_GOOGLELOGIN_URL;
-export const KAKAO_URL = import.meta.env.VITE_KAKAOLOGIN_URL;
+export const BACKEND_URL = process.env.VITE_BACKEND_URL;
+export const GOOGLE_URL = process.env.VITE_GOOGLELOGIN_URL;
+export const KAKAO_URL = process.env.VITE_KAKAOLOGIN_URL;
 const accessToken = localStorage.getItem('accessToken');
 
 export const instance = axios.create({
