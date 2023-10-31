1391f0946516e17e0f3e94c293ae6f2075549dc2
kimyunho <31121701+ua1it@users.noreply.github.com>
2023-10-31 23:11:17 +0900

    refactor: 필요 없는 파일 삭제

--- src/test/Example.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Example from '@/pages/Example';
-
-test('불러오기 테스트페이지', () => {
-  render(<Example />);
-  const buttonElement = screen.getByText('버튼');
-  expect(buttonElement).toBeInTheDocument();
-});
