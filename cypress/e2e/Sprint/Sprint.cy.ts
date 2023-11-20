aef1701704d8355bb5fae59b421e6cdbc7df6092
jomo34 <trillion32@naver.com>
2023-11-20 23:46:46 +0900

    test: 스프린트 추가하기, 하위작업 생성 테스트코드 작성 (미완성)

--- cypress/e2e/Sprint/Sprint.cy.ts
+++ cypress/e2e/Sprint/Sprint.cy.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 describe('작업관리 페이지 테스트', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/projects', {
@@ -6,6 +8,10 @@ describe('작업관리 페이지 테스트', () => {
     cy.intercept('GET', '/sprints?projectId=5', {
       fixture: 'sprintContents.json',
     }).as('sprintContents');
+    cy.setCookie(
+      'accessToken',
+      'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjk5NDQyNjQwLCJleHAiOjE2OTk0NDM1NDB9.WVj3PdtRj8SBg2xOSTvM1ZK8hJRheMxiiAR3bFir6w3v4IDe-MmMgQx1BdSPqAXQIeGDiAjyP-_YPrX1MK4hUA'
+    );
     cy.visit(`/projects/5/sprints`);
   });
 
@@ -50,7 +56,17 @@ describe('작업관리 페이지 테스트', () => {
       cy.get('[data-row-key="1"] > [style="background: rgb(35, 196, 131);"]').should('be.visible');
     });
 
-    it('스프린트 추가하기 버튼을 누르면 스프린트 추가하기 모달이 표시되어야 한다.', () => {
+    it('각 스프린트의 + 버튼을 누르면 세부 할일들이 표시되어야 한다.', () => {
+      cy.get('[data-row-key="0"] > .ant-table-cell-fix-left > .ant-table-row-expand-icon').click();
+      cy.get(':nth-child(3) > .ant-table-cell-fix-left > div').should('have.text', '회원가입');
+      cy.get(':nth-child(4) > .ant-table-cell-fix-left > div').should('have.text', '로그인');
+      cy.get('[data-row-key="1"] > .ant-table-cell-fix-left > .ant-table-row-expand-icon').click();
+      cy.get(':nth-child(6) > .ant-table-cell-fix-left > div').should('have.text', '상품 조회');
+    });
+  });
+
+  context('스프린트 추가하기 버튼을 누를경우', () => {
+    it('스프린트 추가하기 모달이 표시되어야 한다.', () => {
       cy.get('.css-k6p0i0 > :nth-child(3)').click();
       cy.get('.ant-modal-content').should('be.visible');
       cy.get('[placeholder="스프린트 명"]').should('be.visible');
@@ -59,12 +75,73 @@ describe('작업관리 페이지 테스트', () => {
       cy.get('.ant-btn-primary').should('have.text', 'OK');
     });
 
-    it('각 스프린트의 + 버튼을 누르면 세부 할일들이 표시되어야 한다.', () => {
-      cy.get('[data-row-key="0"] > .ant-table-cell-fix-left > .ant-table-row-expand-icon').click();
-      cy.get(':nth-child(3) > .ant-table-cell-fix-left > div').should('have.text', '회원가입');
-      cy.get(':nth-child(4) > .ant-table-cell-fix-left > div').should('have.text', '로그인');
-      cy.get('[data-row-key="1"] > .ant-table-cell-fix-left > .ant-table-row-expand-icon').click();
-      cy.get(':nth-child(6) > .ant-table-cell-fix-left > div').should('have.text', '상품 조회');
+    it('생성에 실패할 경우 에러 메시지가 상단에 보여야 한다.', () => {
+      cy.get('.css-k6p0i0 > :nth-child(3)').click();
+      cy.get('.ant-btn-primary').click();
+      cy.get('.Toastify__toast-body > :nth-child(2)').should('have.text', '스프린트 생성에 실패했습니다.');
+    });
+
+    it('생성에 성공한 경우 성공 메시지가 상단에 보여야 한다.', () => {
+      const today = dayjs().format('YYYY-MM-DD');
+
+      cy.get('.css-k6p0i0 > :nth-child(3)').click();
+      cy.get('[placeholder="스프린트 명"]').type('테스트');
+      cy.get('[placeholder="스프린트 설명"]').type('테스트 입니다.');
+      cy.get(':nth-child(3) > .ant-picker').type(`${today}{enter}`);
+      cy.get(':nth-child(4) > .ant-picker').type(`${today}{enter}`);
+      cy.get('.ant-btn-primary').should('have.text', 'OK').click();
+      cy.intercept({
+        method: 'post',
+        url: 'http://localhost:8080/sprints',
+      }).as('createSprint');
+      // cy.intercept('POST', 'http://localhost:8080/sprints', req => {
+      //   const { body } = req;
+      //   req.continue(res => {
+      //     res.body.data.listBankAccount = [
+      //       {
+      //         message: '테스크가 생성되었습니다.',
+      //       },
+      //     ];
+      //   });
+      // });
+      //스프린트 생성 미완성
+      //cy.get('.Toastify__toast-body > :nth-child(2)').should('have.text', '스프린트 생성에 성공했습니다.');
+    });
+  });
+
+  context('할일 추가 버튼을 누를경우', () => {
+    it('새 하위 작업 생성 모달이 표시되어야 한다.', () => {
+      cy.get(
+        '[data-row-key="0"] > .ant-table-cell-fix-left > [style="display: flex; justify-content: space-between; align-items: center;"] > .ant-btn'
+      ).click();
+      cy.get('.ant-modal-content').should('be.visible');
+      cy.get('[placeholder="하위 작업 명"]').should('be.visible');
+      cy.get('[placeholder="하위 작업 설명"]').should('be.visible');
+      cy.get('.ant-modal-footer > .ant-btn-default').should('have.text', 'Cancel');
+      cy.get('.ant-btn-primary').should('have.text', 'OK');
+    });
+
+    it('생성에 실패할 경우 에러 메시지가 상단에 보여야 한다.', () => {
+      cy.get(
+        '[data-row-key="0"] > .ant-table-cell-fix-left > [style="display: flex; justify-content: space-between; align-items: center;"] > .ant-btn'
+      ).click();
+      cy.get('.ant-btn-primary').click();
+      cy.get('.Toastify__toast-body > :nth-child(2)').should('have.text', '하위작업 생성에 실패했습니다.');
+    });
+
+    it('생성에 성공한 경우 성공 메시지가 상단에 보여야 한다.', () => {
+      cy.get(
+        '[data-row-key="0"] > .ant-table-cell-fix-left > [style="display: flex; justify-content: space-between; align-items: center;"] > .ant-btn'
+      ).click();
+      cy.get('[placeholder="하위 작업 명"]').type('테스트');
+      cy.get('[placeholder="하위 작업 설명"]').type('테스트 입니다.');
+      cy.get('.ant-btn-primary').click();
+      cy.intercept({
+        method: 'post',
+        url: 'http://localhost:8080/tasks',
+      }).as('createTask');
+      //하위작업 생성 미완성
+      //cy.get('.Toastify__toast-body > :nth-child(2)').should('have.text', '하위작업 생성에 성공했습니다.');
     });
   });
 });
